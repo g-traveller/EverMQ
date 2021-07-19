d9dd7341719b93ae9dfc4e288366acd207511275
Nan Jiang (EE-CN-29) <Nan.JA.Jiang@bmw.com>
2021-07-18 22:51:10 -0400

    update class name

--- src/main/java/org/everteam/evermq/EverMqApplication.java
+++ src/main/java/org/everteam/evermq/EverMqApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package org.everteam.evermq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class EverMqApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EverMqApplication.class, args);
 	}
 
 }
